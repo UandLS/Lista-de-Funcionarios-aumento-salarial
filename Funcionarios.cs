b89a5e09c1da21d8aec8dd1e69212fffc0945d4d
UandLS <126968855+UandLS@users.noreply.github.com>
2023-10-31 22:16:36 -0300

    Add files via upload
    Projeto que cria uma lista de funcionários com um ID, nome e salario para cada um. Além disso, com base no ID, o programa permite adicionar um percentual definido pelo usuário ao salario de um dos funcionários.

---
+++ Funcionarios.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Aumento_Salario
+{
+
+public class Funcionarios
+{
+    public int ID { get; set; }
+    public string Nome { get; set; }
+    public double Salario { get; private set; }
+    public double Aumento { get; set; }
+
+
+    public Funcionarios(int id, string nome, double salario)
+    {
+        ID = id;
+        Nome = nome;
+        Salario = salario;
+
+    }
+
+
+
+    public void Idadd(double percent)
+    {
+
+        Salario += Salario * (percent / 100);
+
+    }
+
+    public override string ToString()
+    {
+
+        return "Funcionario: ID " + ID + " Nome: " + Nome + " R$: " + Salario;
+    }
+
+}
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using Aumento_Salario;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+namespace Aumento_Salario
+{
+
+    public static class Program
+    {
+        public static void Main()
+        {
+
+            Console.Write("Quantos fucionarios para registrar ");
+            int nfunc = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            List<Funcionarios> ids = new List<Funcionarios>();
+
+            for (int i = 0; i < nfunc; i++)
+            {
+
+                Console.WriteLine("Digite a ID do funcionario");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+
+                Console.WriteLine("Nome do funcionario");
+                string nome = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Salario do funcionario");
+                double salario = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                ids.Add(new Funcionarios(id, nome, salario));
+
+                foreach (Funcionarios objt in ids)
+                {
+                    Console.WriteLine(objt);
+                }
+            }
+
+                Console.WriteLine();
+                Console.WriteLine("Digite a Id do funcionario que receberá aumento");
+                int Idadd = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Funcionarios Aum = ids.Find(x => x.ID == Idadd);
+
+                if (Aum != null)
+                {
+
+                    Console.Write("entre com a porcentagem: ");
+                    double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Aum.Idadd(percent);
+                }
+                else
+                {
+                    Console.WriteLine("Id Invalido");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Salario atualizado: ");
+                foreach (Funcionarios objt in ids)
+                {
+                    Console.WriteLine(objt);
+                }
+
+
+
+
+            
+        }
+    }
+}
